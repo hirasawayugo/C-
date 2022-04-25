8762a0683f24a9d37593ff06bb143340e360df27
hirasawayugo <103486065+hirasawayugo@users.noreply.github.com>
2022-04-25 11:33:31 +0900

    更新202204251133

--- Matrix Test/Matrix33.cpp
+++ Matrix Test/Matrix33.cpp
@@ -10,9 +10,15 @@ Matrix33::~Matrix33()
 
 void Matrix33::setValue(float *x, float *y, float *z)
 {
-	value[0][0] = *x;
-	value[1][0] = *y;
-	value[2][0] = *z;
+	value[0][0] = x[0];
+	value[0][1] = x[1];
+	value[0][2] = x[2];
+	value[1][0] = y[0];
+	value[1][1] = y[1];
+	value[1][2] = y[2];
+	value[2][0] = z[0];
+	value[2][1] = z[1];
+	value[2][2] = z[2];
 }
 float** Matrix33::getValue() const
 {
