1736ad33bf1f681b3ab96a4b77f894a53bfd6c60
hirasawayugo <103486065+hirasawayugo@users.noreply.github.com>
2022-04-27 14:54:34 +0900

    更新202204271454

--- C# Matrix test/Program.cs
+++ C# Matrix test/Program.cs
@@ -6,17 +6,17 @@ namespace Matrix_test
     {
         static void Main()
         {
-            Matrix mat = new Matrix();
-            int [,]mn = new int [,]{ {1,2},{3,4} };
-            int [,]np = new int [,]{ {10,20},{30,40} };
-            int [,]value = mat.mult(mn, np);
+            Matrix33 mat33_1 = new Matrix33(1,2,3,4,5,6,7,8,9 );
+            Matrix33 mat33_2 = new Matrix33(10,20,30,40,50,60,70,80,90 );
 
-            for (int i = 0; i < 2; i++ ) 
+            int [,]mn44 = { {1,2,3,4},{5,6,7,8},{9,10,11,12}, {13,14,15,16} };
+            int [,]np44 = { {10,20,30,40},{50,60,70,80},{90,100,110,120}, {130,140,150,160} };
+            Matrix33 mult33 = mat33_1 * mat33_2;
+
+            Console.WriteLine("3*3");
+            for (int i = 0; i < 3; i++ ) 
             {
-                for (int j = 0; j < 2; j++ ) 
-                {
-                    Console.WriteLine(value[i,j]);
-                }
+                Console.WriteLine("{0} {1} {2}",mult33[i * 3], mult33[i * 3 + 1], mult33[i * 3 + 2]);
             }
         }
     }
---
+++ C# Matrix test/matrix33.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Runtime.Serialization;
+[DataContract]
+class Matrix33
+{
+    public Matrix33( float m11, float m12, float m13, float m21, float m22, float m23, float m31, float m32, float m33 ){
+        M11 = m11;
+        M12 = m12;
+        M13 = m13;
+        M21 = m21;
+        M22 = m22;
+        M23 = m23;
+        M31 = m31;
+        M32 = m32;
+        M33 = m33; 
+    }
+    private static int SIZE = 3;
+    [DataMember]
+    public float M11;
+    [DataMember]
+    public float M12;
+    [DataMember]
+    public float M13;
+    [DataMember]
+    public float M21;
+    [DataMember]
+    public float M22;
+    [DataMember]
+    public float M23;
+    [DataMember]
+    public float M31;
+    [DataMember]
+    public float M32;
+    [DataMember]
+    public float M33;
+    
+    public float this[int index]{
+        get
+        {
+            switch(index){
+                case 0: return M11;
+                case 1: return M12;
+                case 2: return M13;
+                case 3: return M21;
+                case 4: return M22;
+                case 5: return M23;
+                case 6: return M31;
+                case 7: return M32;
+                case 8: return M33;
+            }
+            return 0;
+        }
+        set
+        {
+             switch(index){
+                case 0: M11 = value; break;
+                case 1: M12 = value; break;
+                case 2: M13 = value; break;
+                case 3: M21 = value; break;
+                case 4: M22 = value; break;
+                case 5: M23 = value; break;
+                case 6: M31 = value; break;
+                case 7: M32 = value; break;
+                case 8: M33 = value; break;
+            }
+        }
+    }
+    public float this[int row, int column]
+    {
+        get
+        {
+            return this[(row * 4) + column];
+        }
+
+        set
+        {
+            this[(row * 4) + column] = value;
+        }
+    }
+
+    public static Matrix33 Multiply(Matrix33 matrix1, Matrix33 matrix2)
+    {
+        var m11 = ((matrix1.M11 * matrix2.M11) + (matrix1.M12 * matrix2.M21)) + (matrix1.M13 * matrix2.M31);
+        var m12 = ((matrix1.M11 * matrix2.M12) + (matrix1.M12 * matrix2.M22)) + (matrix1.M13 * matrix2.M32);
+        var m13 = ((matrix1.M11 * matrix2.M13) + (matrix1.M12 * matrix2.M23)) + (matrix1.M13 * matrix2.M33);
+        var m21 = ((matrix1.M21 * matrix2.M11) + (matrix1.M22 * matrix2.M21)) + (matrix1.M23 * matrix2.M31);
+        var m22 = ((matrix1.M21 * matrix2.M12) + (matrix1.M22 * matrix2.M22)) + (matrix1.M23 * matrix2.M32);
+        var m23 = ((matrix1.M21 * matrix2.M13) + (matrix1.M22 * matrix2.M23)) + (matrix1.M23 * matrix2.M33);
+        var m31 = ((matrix1.M31 * matrix2.M11) + (matrix1.M32 * matrix2.M21)) + (matrix1.M33 * matrix2.M31);
+        var m32 = ((matrix1.M31 * matrix2.M12) + (matrix1.M32 * matrix2.M22)) + (matrix1.M33 * matrix2.M32);
+        var m33 = ((matrix1.M31 * matrix2.M13) + (matrix1.M32 * matrix2.M23)) + (matrix1.M33 * matrix2.M33);
+        matrix1.M11 = m11;
+        matrix1.M12 = m12;
+        matrix1.M13 = m13;
+        matrix1.M21 = m21;
+        matrix1.M22 = m22;
+        matrix1.M23 = m23;
+        matrix1.M31 = m31;
+        matrix1.M32 = m32;
+        matrix1.M33 = m33;
+        return matrix1;
+    }
+
+    public static void Multiply( ref Matrix33 matrix1, ref Matrix33 matrix2, out Matrix33 result )
+    {
+        var m11 = ((matrix1.M11 * matrix2.M11) + (matrix1.M12 * matrix2.M21)) + (matrix1.M13 * matrix2.M31);
+        var m12 = ((matrix1.M11 * matrix2.M12) + (matrix1.M12 * matrix2.M22)) + (matrix1.M13 * matrix2.M32);
+        var m13 = ((matrix1.M11 * matrix2.M13) + (matrix1.M12 * matrix2.M23)) + (matrix1.M13 * matrix2.M33);
+        var m21 = ((matrix1.M21 * matrix2.M11) + (matrix1.M22 * matrix2.M21)) + (matrix1.M23 * matrix2.M31);
+        var m22 = ((matrix1.M21 * matrix2.M12) + (matrix1.M22 * matrix2.M22)) + (matrix1.M23 * matrix2.M32);
+        var m23 = ((matrix1.M21 * matrix2.M13) + (matrix1.M22 * matrix2.M23)) + (matrix1.M23 * matrix2.M33);
+        var m31 = ((matrix1.M31 * matrix2.M11) + (matrix1.M32 * matrix2.M21)) + (matrix1.M33 * matrix2.M31);
+        var m32 = ((matrix1.M31 * matrix2.M12) + (matrix1.M32 * matrix2.M22)) + (matrix1.M33 * matrix2.M32);
+        var m33 = ((matrix1.M31 * matrix2.M13) + (matrix1.M32 * matrix2.M23)) + (matrix1.M33 * matrix2.M33);
+        result.M11 = m11;
+        result.M12 = m12;
+        result.M13 = m13;
+        result.M21 = m21;
+        result.M22 = m22;
+        result.M23 = m23;
+        result.M31 = m31;
+        result.M32 = m32;
+        result.M33 = m33;
+    }
+
+    public static Matrix33 operator*( Matrix33 matrix1, Matrix33 matrix2){
+        return Multiply( matrix1, matrix2);
+    }
+
+    public static Matrix33 Multply( Matrix33 matrix1, float scaleFactor ){
+        matrix1.M11 *= scaleFactor;
+        matrix1.M12 *= scaleFactor;
+        matrix1.M13 *= scaleFactor;
+        matrix1.M21 *= scaleFactor;
+        matrix1.M22 *= scaleFactor;
+        matrix1.M23 *= scaleFactor;
+        matrix1.M31 *= scaleFactor;
+        matrix1.M32 *= scaleFactor;
+        matrix1.M33 *= scaleFactor;
+        return matrix1;
+    }
+
+}
